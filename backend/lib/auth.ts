9602ed32e142165a76e164c18a438370fe17ea8e
fjohanssonnn <fjohansson.creative@gmail.com>
2025-11-20 23:06:46 +0100

    feat: added open-api specs

--- backend/lib/auth.ts
+++ backend/lib/auth.ts
@@ -1,5 +1,6 @@
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
+import { openAPI } from "better-auth/plugins"
 import { db } from "./db";
 
 export const auth = betterAuth({
@@ -9,4 +10,7 @@ export const auth = betterAuth({
   emailAndPassword: {
     enabled: true,
   },
+  plugins: [
+    openAPI()
+  ]
 }); 

3faf69196392c05581060e60e67a9f3c0cf3b57f
fjohanssonnn <fjohansson.creative@gmail.com>
2025-11-24 20:18:50 +0100

    feat: added beta-badge

--- frontend/src/components/header.tsx
+++ frontend/src/components/header.tsx
@@ -1,10 +1,14 @@
 import { Link } from "@tanstack/react-router";
+import { Badge } from "@/components/ui/badge";
 
 export function Header() {
   return (
     <header className="h-14 border-b">
       <div className="flex h-full items-center justify-between container mx-auto px-4">
-        <Link to="/">Ember</Link>
+        <Link className="flex space-x-2" to="/">
+          <span>Ember</span>
+          <Badge className="w-10 h-4">Beta</Badge>
+        </Link>
         <nav>
           <ul className="flex space-x-4 text-sm">
             <li>

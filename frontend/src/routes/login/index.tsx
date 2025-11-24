1e4b49fd3766a87421adeee77e88fe900e691153
fjohanssonnn <fjohansson.creative@gmail.com>
2025-11-24 18:01:14 +0100

    feat: added login form

--- frontend/src/routes/login/index.tsx
+++ frontend/src/routes/login/index.tsx
@@ -1,9 +1,112 @@
 import { createFileRoute } from "@tanstack/react-router";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Container } from "@/components/container";
+import { useForm } from "@tanstack/react-form";
+import { z } from "zod";
+import { Field, FieldGroup, FieldLabel } from "@/components/ui/field";
+import { authClient } from "@/lib/auth-client";
 
 export const Route = createFileRoute("/login/")({
   component: LoginComponent,
 });
 
+const formSchema = z.object({
+  email: z.email(),
+  password: z.string(),
+});
+
 function LoginComponent() {
-  return <div>Hello "/login/"!</div>;
+  const form = useForm({
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+    onSubmit: ({ value }) =>
+      signIn.email({
+        email: value.email,
+        password: value.password,
+        callbackURL: "/dashboard",
+      }),
+    validators: {
+      onSubmit: formSchema,
+    },
+  });
+
+  const { signIn } = authClient;
+
+  return (
+    <main className="flex items-center min-h-[calc(100vh_-_3.5rem)]">
+      <Container className="justify-center">
+        <Card className="w-full max-w-sm">
+          <CardHeader>
+            <CardTitle>Login to your account</CardTitle>
+            <CardDescription>
+              Enter your email below to login to your account
+            </CardDescription>
+          </CardHeader>
+          <CardContent>
+            <form
+              id="login-form"
+              onSubmit={(e) => {
+                e.preventDefault();
+                form.handleSubmit();
+              }}
+            >
+              <FieldGroup>
+                <form.Field
+                  name="email"
+                  children={(field) => (
+                    <Field>
+                      <FieldLabel htmlFor={field.name}>Email</FieldLabel>
+                      <Input
+                        id={field.name}
+                        value={field.state.value}
+                        onBlur={field.handleBlur}
+                        onChange={(e) => field.handleChange(e.target.value)}
+                        placeholder="john.doe@gmail.com"
+                        required
+                      />
+                    </Field>
+                  )}
+                />
+                <form.Field
+                  name="password"
+                  children={(field) => (
+                    <Field>
+                      <FieldLabel htmlFor={field.name}>Password</FieldLabel>
+                      <Input
+                        id={field.name}
+                        onBlur={field.handleBlur}
+                        onChange={(e) => field.handleChange(e.target.value)}
+                        placeholder="*******"
+                        type="password"
+                        required
+                      />
+                    </Field>
+                  )}
+                />
+              </FieldGroup>
+            </form>
+          </CardContent>
+          <CardFooter className="flex-col gap-2">
+            <Button form="login-form" type="submit" className="w-full">
+              Login
+            </Button>
+            <Button variant="outline" className="w-full">
+              Login with Google
+            </Button>
+          </CardFooter>
+        </Card>
+      </Container>
+    </main>
+  );
 }

01af34e969d9133da920b0c67a3ae231ad66b34c
fjohanssonnn <fjohansson.creative@gmail.com>
2025-11-24 10:19:28 +0100

    feat: added eden and tanstack query

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,22 +1,12 @@
 import { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
-import { RouterProvider, createRouter } from "@tanstack/react-router";
-
-// Import the generated route tree
-import { routeTree } from "./routeTree.gen";
+import { RouterProvider } from "@tanstack/react-router";
 
 import "./styles.css";
 import reportWebVitals from "./reportWebVitals.ts";
+import { getRouter } from "./router.tsx";
 
-// Create a new router instance
-const router = createRouter({
-  routeTree,
-  context: {},
-  defaultPreload: "intent",
-  scrollRestoration: true,
-  defaultStructuralSharing: true,
-  defaultPreloadStaleTime: 0,
-});
+const router = getRouter();
 
 // Register the router instance for type safety
 declare module "@tanstack/react-router" {

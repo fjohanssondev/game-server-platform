4b5b7b30324c7ac48c5120674425d6b3e62a8a42
fjohanssonnn <fjohansson.creative@gmail.com>
2025-11-24 12:59:14 +0100

    feat: lagt till cookies

--- frontend/src/lib/eden.ts
+++ frontend/src/lib/eden.ts
@@ -1,4 +1,8 @@
 import { treaty } from "@elysiajs/eden";
 import type { App } from "../../../backend/src/index";
 
-export const api = treaty<App>("localhost:3000");
+export const api = treaty<App>("localhost:3000", {
+  fetch: {
+    credentials: true,
+  },
+});

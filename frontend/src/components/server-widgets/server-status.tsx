a5ae4151b43232e2a8793927600ba00d01f5bff9
fjohanssonnn <fjohansson.creative@gmail.com>
2025-11-25 11:36:59 +0100

    fix: labels

--- frontend/src/components/server-widgets/server-status.tsx
+++ frontend/src/components/server-widgets/server-status.tsx
@@ -49,15 +49,28 @@ export function ServerStatus(server: Server) {
             <span>4d</span>
           </li>
           <li className="flex flex-col space-y-2">
-            <p className="text-sm">CPU</p>
+            <div className="flex justify-between items-center">
+              <p className="text-sm">CPU</p>
+              <div className="flex space-x-1">
+                <p className="text-xs">{data?.cpu.percent}</p>
+                <span className="text-xs">/</span>
+                <p className="text-xs">100%</p>
+              </div>
+            </div>
             <Progress value={Number(data?.cpu.percent)} />
           </li>
           <li className="flex flex-col space-y-2">
             <div className="flex justify-between items-center">
               <p className="text-sm">RAM</p>
-              <p className="text-xs">
-                {(Number(data?.memory.limit) / 1000).toFixed(2)} GB
-              </p>
+              <div className="flex space-x-1">
+                <p className="text-xs">
+                  {(Number(data?.memory.usage) / 1000).toFixed(2)}
+                </p>
+                <span className="text-xs">/</span>
+                <p className="text-xs">
+                  {(Number(data?.memory.limit) / 1000).toFixed(2)} GB
+                </p>
+              </div>
             </div>
             <Progress value={Number(data?.memory.percent)} />
           </li>
